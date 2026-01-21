d80d64e7bf39450781649c428f1986b232dbfcaf
vignesh <3470test@gmail.com>
2026-01-21 09:44:25 +0530

    add

--- config.js
+++ config.js
@@ -2,4 +2,4 @@ const API_BASE_URL =
   window.location.hostname === "localhost" ||
   window.location.hostname === "127.0.0.1"
     ? "http://localhost:3000"
-    : "https://www.3470healthcare.org";
+    : "https://api.3470healthcare.org";
